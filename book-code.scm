af6224c191120890064eab35d217343711d47480
Tom O'Brien <thomas.obrien86@gmail.com>
2012-05-14 13:23:19 +0100

    Even more symbolic differentiation

--- book-code.scm
+++ book-code.scm
@@ -344,4 +344,57 @@
           (else
            (error "unknown expression type -- DERIV" exp)))))
 
+(define variable?
+  (lambda (x)
+    (symbol? x)))
 
+(define same-variable?
+  (lambda (v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2))))
+
+(define =number?
+  (lambda (exp num)
+    (and (number? exp) (= exp num))))
+
+(define make-sum
+  (lambda (a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2)) (+ a1 a2))
+          (else (list '+ a1 a2)))))
+
+(define make-product
+  (lambda (m1 m2)
+    (list '* m1 m2)))
+
+(define make-product
+  (lambda (m1 m2)
+    (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+          ((=number? m1 1) m2)
+          ((=number? m2 1) m1)
+          ((and (number? m1) (number? m2)) (* m1 m2))
+          (else (list '* m1 m2)))))
+
+(define sum?
+  (lambda (x)
+    (and (pair? x) (eq? (car x) '+))))
+
+(define addend
+  (lambda (s)
+    (cadr s)))
+
+(define augend
+  (lambda (s)
+    (caddr s)))
+
+(define product?
+  (lambda (x)
+    (and (pair? x) (eq? (car x) '*))))
+
+(define multiplier
+  (lambda (p)
+    (cadr p)))
+
+(define multiplicand
+  (lambda (p)
+    (caddr p)))
